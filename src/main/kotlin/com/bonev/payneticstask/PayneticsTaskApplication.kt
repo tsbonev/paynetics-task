5d3958109bc33fa358543c24ca48b5466d874faf
Tsvetozar Bonev <tsbonev@gmail.com>
2023-11-03 17:56:15 +0200

    initial commit

---
+++ src/main/kotlin/com/bonev/payneticstask/PayneticsTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.bonev.payneticstask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayneticsTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<PayneticsTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/bonev/payneticstask/PayneticsTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bonev.payneticstask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayneticsTaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
